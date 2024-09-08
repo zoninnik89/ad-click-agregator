c4ad9612d9e65aa1a67c984e96484fe6d105cc09
Nick Zonin <zoninnik@gmail.com>
2024-09-08 15:50:27 +0200

    Third commit

--- gateway/go.mod
+++ gateway/go.mod
@@ -1,3 +1,8 @@
 module github.com/zoninnik/ad-click-aggregator/gateway
 
 go 1.23.1
+
+require (
+	github.com/joho/godotenv v1.5.1
+	github.com/zoninnik89/commons v0.0.0-20240908125020-0d141c3cdbde
+)
--- gateway/main.go
+++ gateway/main.go
@@ -3,10 +3,13 @@ package main
 import (
 	"log"
 	"net/http"
+
+	_ "github.com/joho/godotenv/autoload"
+	common "github.com/zoninnik89/commons"
 )
 
-const (
-	httpAddress = ":8080"
+var (
+	httpAddress = common.EnvString("HTTP_ADDR", ":8080")
 )
 
 func main() {
